dd64d32fb9cd1c4898f41ad262562fba22627543
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-04-19 11:47:44 +0200

    fix(manifest): remove qb-target as a dependency

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,34 +7,33 @@ version '1.0.0'
 ui_page 'html/meter.html'
 
 shared_scripts {
-    '@qb-core/shared/locale.lua',
-    'locales/en.lua', -- Change to the language you want to use
-    'config.lua',
+	'@qb-core/shared/locale.lua',
+	'locales/en.lua', -- Change to the language you want to use
+	'config.lua',
 }
 
 dependencies {
 	'qb-core',
 	'PolyZone',
-	'qb-target',
 }
 
-client_scripts { 
-    '@PolyZone/client.lua',
+client_scripts {
+	'@PolyZone/client.lua',
 	'@PolyZone/BoxZone.lua',
 	'@PolyZone/EntityZone.lua',
 	'@PolyZone/CircleZone.lua',
 	'@PolyZone/ComboZone.lua',
-
-    'client/main.lua',
+	'client/main.lua',
 }
+
 server_script 'server/main.lua'
 
 files {
-    'html/meter.css',
-    'html/meter.html',
-    'html/meter.js',
-    'html/reset.css',
-    'html/g5-meter.png'
+	'html/meter.css',
+	'html/meter.html',
+	'html/meter.js',
+	'html/reset.css',
+	'html/g5-meter.png'
 }
 
 lua54 'yes'
