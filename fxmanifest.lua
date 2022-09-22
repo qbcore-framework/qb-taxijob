2b35f3e47c53d71298bd9ac30be383c34a197176
buddizer <none>
2022-09-22 09:50:06 -0600

    auto switch locale system

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,34 +7,34 @@ version '1.0.0'
 ui_page 'html/meter.html'
 
 shared_scripts {
-	'@qb-core/shared/locale.lua',
-	'locales/en.lua',
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
     'locales/*.lua',
-	'config.lua',
+    'config.lua',
 }
 
 dependencies {
-	'qb-core',
-	'PolyZone',
+    'qb-core',
+    'PolyZone',
 }
 
 client_scripts {
-	'@PolyZone/client.lua',
-	'@PolyZone/BoxZone.lua',
-	'@PolyZone/EntityZone.lua',
-	'@PolyZone/CircleZone.lua',
-	'@PolyZone/ComboZone.lua',
-	'client/main.lua',
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/EntityZone.lua',
+    '@PolyZone/CircleZone.lua',
+    '@PolyZone/ComboZone.lua',
+    'client/main.lua',
 }
 
 server_script 'server/main.lua'
 
 files {
-	'html/meter.css',
-	'html/meter.html',
-	'html/meter.js',
-	'html/reset.css',
-	'html/g5-meter.png'
+    'html/meter.css',
+    'html/meter.html',
+    'html/meter.js',
+    'html/reset.css',
+    'html/g5-meter.png'
 }
 
 lua54 'yes'
