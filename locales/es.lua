98b3839a83ac276eb2f40a101b39abc043964b05
jojutom <159322976+jojutom@users.noreply.github.com>
2024-04-27 13:26:59 +0200

    es.lua

--- locales/es.lua
+++ locales/es.lua
@@ -23,7 +23,7 @@ local Translations = {
         ["no_spawn_point"] = "No es posible encontrar una ubicación para traer el taxi",
         ["taxi_returned"] = "Taxi estacionado",
         ["on_duty"] = "[E] - Go on duty",
-        ["on_duty"] = "[E] - Go off duty"
+        ["off_duty"] = "[E] - Go off duty"
     },
     menu = {
         ["taxi_menu_header"] = "Vehículos para taxi",
