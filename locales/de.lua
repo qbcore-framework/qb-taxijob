06e7859d4f5c1939793ed83d1decdbc40a5f7b5b
Marcell <chillcode.codersparadise@gmail.com>
2024-09-12 17:30:17 +0200

    added missing variables and fixed spelling errors

--- locales/de.lua
+++ locales/de.lua
@@ -1,32 +1,43 @@
 local Translations = {
     error = {
-        ['already_mission'] = 'Du bist bereits in einer NPC-Mission',
-        ['not_in_taxi'] = 'Sie sind nicht in einem Taxi',
-        ['missing_meter'] = 'Dieses Fahrzeug hat keine Taxameter',
-        ['no_vehicle'] = 'Sie sind nicht in einem Fahrzeug',
-        ['not_active_meter'] = 'Das Taxameter ist nicht aktiv',
+        ['already_mission'] = 'Du machst bereits eine NPC-Mission',
+        ['not_in_taxi'] = 'Du bist nicht in einem Taxi',
+        ['missing_meter'] = 'Dieses Fahrzeug hat keinen Taxameter',
+        ['no_vehicle'] = 'Du bist nicht in einem Fahrzeug',
+        ['not_active_meter'] = 'Der Taxameter ist nicht aktiv',
+        ['ride_canceled'] = 'Du hattest zu viele Unfälle, die Fahrt wurde abgebrochen!',
+        ['broken_taxi'] = 'Dein Taxi muss repariert werden, bevor du die Arbeit fortsetzen kannst!',
+        ['crash_warning'] = 'Wenn du noch %d Mal %s einen Unfall baust, wird der Kunde die Fahrt abbrechen und du wirst nicht bezahlt!',
+        ['time'] = 'Mal',
+        ['times'] = 'Mal'
+    },
+    success = {
+        ['mission_cancelled'] = 'Mission abgebrochen'
     },
     info = {
         ['person_was_dropped_off'] = 'Person wurde abgesetzt!',
-        ['npc_on_gps'] = 'Der NPC wird auf Ihrem GPS angezeigt',
-        ['go_to_location'] = 'Den NPC an den angegebenen Ort bringen',
+        ['npc_on_gps'] = 'Der NPC ist auf deinem GPS markiert',
+        ['go_to_location'] = 'Bringe den NPC zum angegebenen Ort',
         ['vehicle_parking'] = '[E] Fahrzeug parken',
         ['job_vehicles'] = '[E] Arbeitsfahrzeug',
         ['drop_off_npc'] = '[E] NPC absetzen',
         ['call_npc'] = '[E] NPC anrufen',
         ['blip_name'] = 'Taxigesellschaft',
         ['taxi_label_1'] = 'Standard Taxi',
-        ['no_spawn_point'] = 'Unfähig, einen Ort zu finden, an den das Taxi gebracht werden kann',
-        ['taxi_returned'] = 'Taxi einparken',
-        ['on_duty'] = '[E] - Go on duty',
-        ['off_duty'] = '[E] - Go off duty'
+        ['no_spawn_point'] = 'Es konnte kein Ort gefunden werden, um das Taxi zu spawnen',
+        ['taxi_returned'] = 'Taxi geparkt',
+        ['on_duty'] = '[E] - Dienst antreten',
+        ['off_duty'] = '[E] - Dienst beenden',
+        ['tip_received'] = 'Du hast $%d Trinkgeld für deine sichere Fahrt erhalten',
+        ['tip_not_received'] = 'Versuche, das Taxi nicht zu beschädigen, wenn du in Zukunft Trinkgeld erhalten möchtest'
     },
     menu = {
         ['taxi_menu_header'] = 'Taxifahrzeuge',
-        ['close_menu'] = '⬅ Schließe Menü',
-        ['boss_menu'] = 'Boss Menu'
+        ['close_menu'] = '⬅ Menü schließen',
+        ['boss_menu'] = 'Boss Menü'
     }
 }
+
 
 if GetConvar('qb_locale', 'en') == 'de' then
     Lang = Locale:new({
